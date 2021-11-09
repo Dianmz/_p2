9701fb5687c9bd80a346b07a90afb91bf0e23b55
Diana Menendez <dcmc2297@gmail.com>
2021-11-09 17:27:22 -0600

    dockerfile

---
+++ Practica2/frontend/src/helpers/fetch.js
@@ -0,0 +1,21 @@
+const fetchManage = ( endpoint, body, method = 'GET' ) => {
+    const url = 'http://http://34.133.206.223:80/' + endpoint;
+
+    if ( method === 'GET' ) {
+        return fetch( url );
+    } else {
+        return fetch( url, {
+            method,
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify( body )
+        });
+    }
+        
+
+}
+
+export {
+    fetchManage
+}
